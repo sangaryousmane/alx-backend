fb085d2244e7cbb46fb68d2beeb6ee08c5b5a987
sangaryousmane <sangaryousmane@gmail.com>
2024-04-01 10:10:06 +0000

    Hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,17 +39,3 @@ class Server:
 
         index = index_range(page, page_size)
         return self.dataset()[index[0]:index[1]]
-
-    def get_hyper(page: int = 1, page_size: int = 10) -> dict:
-        """ method that takes the same arguments (and defaults)
-        as get_page and returns a dictionary"""
-        data = server.get_data(page, page_size)
-        dataset = server.dataset()
-        len_of_dataset = len(dataset) if dataset else None
-        pages = math.ceil(len_of_dataset / page_size) if dataset else None
-        page_size = len(data) if data else None
-        prev = page - 1 if page > 1 else None
-        next_ = page + 1 if page < pages else None
-
-        return {'page_size': page_size, 'page': page, 'data': data,
-                'next_page': next_, 'prev_page': prev, 'pages': pages}
