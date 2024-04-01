00f68b5c797874689fc91783210e1f34687e0111
sangaryousmane <sangaryousmane@gmail.com>
2024-04-01 07:58:38 +0000

    Write simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Return a range of indexes corresponding pagination
+"""
+
+
+def index_range(page, page_size):
+    """ Return a range of indexes corresponding pagination
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
