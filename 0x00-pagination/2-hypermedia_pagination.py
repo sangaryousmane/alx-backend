65ff8f40dabe782722f980e579e7860a838e764d
sangaryousmane <sangaryousmane@gmail.com>
2024-04-01 10:03:03 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -8,15 +8,14 @@ index_range = __import__('0-simple_helper_function').index_range
 server = Server()
 
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+def get_hyper(page: int = 1, page_size: int = 10) -> dict:
     """ method that takes the same arguments (and defaults)
     as get_page and returns a dictionary"""
 
-    pages = math.ceil()
     data = server.get_data(page, page_size)
     dataset = server.dataset()
     len_of_dataset = len(dataset) if dataset else None
-    pages = math.ciel(len_of_dataset / page_size) if dataset else None
+    pages = math.ceil(len_of_dataset / page_size) if dataset else None
     page_size = len(data) if data else None
     prev = page - 1 if page > 1 else None
     next_ = page + 1 if page < pages else None
