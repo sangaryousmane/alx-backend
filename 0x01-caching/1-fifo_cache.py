22e7dc15bb6e574290ebf8fca73c0615a66a37b3
sangaryousmane <sangaryousmane@gmail.com>
2024-04-03 17:19:38 +0000

    Implement the FIFO cache using Deque

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,6 +2,7 @@
 """ Implement a FIFO cache algorithm
 """
 from base_caching import BaseCaching
+from collections import deque
 
 
 class FIFOCache(BaseCaching):
@@ -10,26 +11,20 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-        self.queue = []
+        self.queue = deque(maxlen=BaseCaching.MAX_ITEMS)
 
     def put(self, key, item):
         """ assign to the dictionary the item value to the key
         """
         if key is None or item is None:
             return
-
-        if key not in self.queue:
-            self.queue.append(key)
-        else:
-            if self.queue[len(self.queue) - 1] != item:
-                self.queue.remove(key)
-                self.queue.append(key)
+        self.queue.append(key)
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            num1 = self.queue.pop(0)
+            num1 = self.queue.popleft()
             del self.cache_data[num1]
-            print("DISCARDS: {}".format(self.cache_data[num1]))
+            print("DISCARDS: {}".format(num1))
 
     def get(self, key):
         """ return the value in the dictionary link to the
