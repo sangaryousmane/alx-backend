47c74c32e94929fd697ef448496ebe19c6a405ea
sangaryousmane <sangaryousmane@gmail.com>
2024-04-03 18:29:22 +0000

    Implement the Least frequently used cache algorithm

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+""" Implement a FIFO cache algorithm
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ Implement a FIFO cache algorithm
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict(max_length=BaseCaching.MAX_ITEMS)
+    
+    def put(self, key, item):
+        """Assign to the dictionary the item value to the key"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = next(iter(self.cache_data))
+            del self.cache_data[lru_key]
+            print("DISCARD: {}".format(lru_key))
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ return the value in the dictionary link to the
+        given key"""
+        if (key is None) or (key not in self.cache_data):
+            return None
+        value = self.cache_data[key]
+        self.cache_data.move_to_end(key)
+        return value
+
+    def print_cache(self):
+        """ Print the cache data"""
+        for key, value in self.cache_data.items():
+            print("{}: {}".format(key, value))
