a41efb134d705ca68292bff70ac040b868b20c50
sangaryousmane <sangaryousmane@gmail.com>
2024-04-03 18:49:05 +0000

    Implement the Least Recently Used Cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -11,7 +11,7 @@ class LRUCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-        self.cache_data = OrderedDict(max_length=BaseCaching.MAX_ITEMS)
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Assign to the dictionary the item value to the key"""
