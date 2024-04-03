77cc5c600a4e5431888f3e7984b457bd1df12f74
sangaryousmane <sangaryousmane@gmail.com>
2024-04-03 17:54:19 +0000

    Fix pycodestyle error

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,27 +2,26 @@
 """ Implement a FIFO cache algorithm
 """
 from base_caching import BaseCaching
-from collections import deque
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """ Implement a FIFO cache algorithm
     """
 
     def __init__(self):
         super().__init__()
-        self.queue = deque(maxlen=BaseCaching.MAX_ITEMS)
+        self.stack = []
 
     def put(self, key, item):
         """Assign to the dictionary the item value to the key"""
         if key is None or item is None:
             return
-
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            oldest_key = self.queue.popleft()
-            del self.cache_data[oldest_key]
-            print("DISCARD: {}".format(oldest_key))
-        self.queue.append(key)
+            newest = self.pop()
+            del self.cache_data[newest]
+            print("DISCARD: {}".format(newest))
+        
+        self.newest.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
