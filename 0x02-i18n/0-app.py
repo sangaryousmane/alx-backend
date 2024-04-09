eedf9f5874c02081646c39ef30d6724ba0b34df5
sangaryousmane <sangaryousmane@gmail.com>
2024-04-09 15:01:16 +0000

    Amend rootn flask app(0-app)

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,15 +2,9 @@
 """ Main Flask app
 """
 from flask import Flask, render_template
-from flask_babel import Babel
 
-
-Config = __import__("./1-app.py").Config
 app = Flask(__name__, template_folder='templates')
-babel = Babel(app)
-app.config.from_object(Config)
 
-@app.route("/", methods=['GET'], strict_slashes=False)
 def hello():
     """ Simple route
     """
