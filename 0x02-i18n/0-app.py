594011b99d0102547a9519991d510b15856d9c98
sangaryousmane <sangaryousmane@gmail.com>
2024-04-09 14:44:41 +0000

    The bare minimum flask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder='templates')
 def hello():
     """ Simple route
     """
-    return render_template("index.html")
+    return render_template("0-index.html")
 
 
 if __name__ == '__main__':
