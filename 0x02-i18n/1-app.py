d21b4c46b0f466cf6ae14e0d89e434c36d6d0498
sangaryousmane <sangaryousmane@gmail.com>
2024-04-09 15:03:30 +0000

    Add Babel, and Configure with a custom class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """ Babel and config class
 """
+from flask_babel import Babel
+from flask import Flask
 
+app = Flask(__name__, template_folder="templates")
+babel = Babel(app)
 
 class Config:
     """ App Configuration class
@@ -9,3 +13,4 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+app.config.from_object(Config)
